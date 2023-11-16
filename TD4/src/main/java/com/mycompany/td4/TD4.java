cd5b36536396c9828862ee6e94ec7e8d46eb102a
Reythias <131203915+Reythias@users.noreply.github.com>
2023-11-16 11:49:52 +0100

    bonjour à tous !

---
+++ TD4/src/main/java/com/mycompany/td4/TD4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.td4;
+
+/**
+ *
+ * @author incen
+ */
+public class TD4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
