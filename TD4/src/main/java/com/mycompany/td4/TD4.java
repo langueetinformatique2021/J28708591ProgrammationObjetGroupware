be76f719c90bfbdd139fc61e5f2d55423ab6372d
Reythias <131203915+Reythias@users.noreply.github.com>
2023-11-16 11:52:21 +0100

    bonjour à tous !

--- TD4/src/main/java/com/mycompany/td4/TD4.java
+++ TD4/src/main/java/com/mycompany/td4/TD4.java
@@ -11,6 +11,6 @@ package com.mycompany.td4;
 public class TD4 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Helwwlo World!");
     }
 }
