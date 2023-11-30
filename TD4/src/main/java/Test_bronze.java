3997de7c8c6b9492993e1b5fa233ec2a2cb6fa13
Reythias <131203915+Reythias@users.noreply.github.com>
2023-11-30 10:01:32 +0100

    bonjour à tous 2

--- TD4/src/main/java/Test_bronze.java
+++ TD4/src/main/java/Test_bronze.java
@@ -1,5 +1,11 @@
 
 public class Test_bronze {
     public static void main(String[] arg){
-            
+      Bronze bronze = new Bronze("metal");
+
+        // Appeler la méthode présente de la classe Félin
+        bronze.présente();
+
+        // Appeler la méthode crie de la classe Félin (qui devrait avoir une implémentation spécifique)  
+}
 }
